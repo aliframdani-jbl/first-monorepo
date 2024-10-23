b8dfd65c9a8bd17cf12604b67ce5d5fd83e4da79
aliframdani-jbl <alif.ramdani@jubelio.com>
2024-10-23 16:55:53 +0700

    chore: comment

--- apps/myapp/src/main.ts
+++ apps/myapp/src/main.ts
@@ -43,7 +43,7 @@ import { UserUsecase } from './usecase/user_usecase';
 
   } catch (error) {
     console.error('Error starting the server:', error);
-    process.exit(1); 
+    process.exit(1); // bertambah
   }
 })();
 
